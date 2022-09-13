5f2e80c0bfd6e50056428f9edc52c3f3adcb04fb
Ilona Shparii <ishparii@google.com>
2022-09-13 15:47:21 +0200

    chore: add ant, less, and signpost-base dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
+const withLess = require('next-with-less');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withLess({
+  ...nextConfig,
+  lessLoaderOptions: {
+    lessOptions: {
+      // See full list of Ant styles here:
+      // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+      modifyVars: {
+        'primary-color': '#51258F',
+      },
+    },
+  },
+});
