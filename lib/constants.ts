5c74433a81099f0c0f12391f1029375e61a61671
Grzegorz Milka <grzegorzmilka@google.com>
2022-09-19 12:48:28 +0200

    feat: migrate some constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,26 @@
 export const SITE_TITLE = 'TODO';
+
+// Cache statically generated pages for 1 hour. The timeout was chosen
+// arbitrarily. Our website has static, non-urgent resources, so we probably do
+// not need to serve content faster.
+export const REVALIDATION_TIMEOUT_SECONDS: number = 1 * 60 * 60;
+
+// The "about us" article ID.
+//
+// TODO
+export const ABOUT_US_ARTICLE_ID: number = 123;
+
+// A mapping from category ID to a Material icon for that category.
+export const CATEGORY_ICON_NAMES: { [key: string]: string } = {
+  /* TODO */
+  '123': 'home_work', // Placeholder
+};
+
+// A list of category IDs that the site should not display.
+export const CATEGORIES_TO_HIDE: number[] = [
+  /* TODO */
+];
+
+export const ZENDESK_AUTH_HEADER = {
+  Authorization: 'Bearer ' + process.env.ZENDESK_OAUTH_TOKEN,
+};
