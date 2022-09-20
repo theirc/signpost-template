01c2dd638bfc8cb7ec02dd90e518563646483a30
Ehimare Okoyomon <eokoyomon@google.com>
2022-09-20 15:06:36 +0200

    feat: add document to template

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          src="https://kit.fontawesome.com/027db3ea3f.js"
+          crossOrigin="anonymous"
+          async
+        ></script>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined"
+          rel="stylesheet"
+        />
+        <script
+          src="https://cdn-eu.readspeaker.com/script/11950/webReader/webReader.js?pids=wr"
+          type="text/javascript"
+          id="rs_req_Init"
+          defer
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
