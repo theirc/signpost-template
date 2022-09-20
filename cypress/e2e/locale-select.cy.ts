05cc94477da0610a32b17438d09adb8fdc2c0fe9
Grzegorz Milka <grzegorzmilka@google.com>
2022-09-20 10:53:42 +0200

    feat: migrate locale select e2e tests

---
+++ cypress/e2e/locale-select.cy.ts
@@ -0,0 +1,20 @@
+describe('locale select', () => {
+  it('redirects based on local storage', () => {
+    localStorage.setItem('preferredLocaleJSON', '"en-us"');
+    cy.visit(encodeURI('http://localhost:3003/locale-select?target=/'));
+    cy.url().should('eq', 'http://localhost:3003/en-us');
+  });
+
+  it('if a user clicks on locale, moves to that page', () => {
+    cy.visit(encodeURI('http://localhost:3003/locale-select?target=/'));
+    cy.get("[href='/en-us']").click();
+    cy.url().should('eq', 'http://localhost:3003/en-us');
+  });
+
+  it('if a user clicks on locale and preferred locale is gibberrish, still works', () => {
+    localStorage.setItem('preferredLocaleJSON', '"rubbish"');
+    cy.visit(encodeURI('http://localhost:3003/locale-select?target=/'));
+    cy.get("[href='/en-us']").click();
+    cy.url().should('eq', 'http://localhost:3003/en-us');
+  });
+});
