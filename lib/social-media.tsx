73d8a57e62eff3d3d7f6aa869afb23891d6b9b09
Ilona Shparii <ishparii@google.com>
2022-09-22 12:06:53 +0200

    fix: pass only serializable strings for social media from getStaticProps

--- lib/social-media.tsx
+++ lib/social-media.tsx
@@ -1,36 +1,42 @@
+// A module for handling social media.
 import { SocialMediaProps } from '@ircsignpost/signpost-base/dist/src/header-banner';
 
 import facebookImage from '../public/facebook.svg';
 import messengerImage from '../public/messenger.svg';
 import whatsappImage from '../public/whatsapp.svg';
 
+export interface SocialMediaLink {
+  title: string;
+  href: string;
+}
+
+// Serializable social media links
+export interface SocialMediaLinks {
+  facebookLink: SocialMediaLink;
+  whatsappLink: SocialMediaLink;
+  messengerLink: SocialMediaLink;
+}
+
 /**
  * Provides data for Social Media buttons, e.g. Facebook, Whatsapp, etc.
  *
- * TODO: Ask your Product manager to get Social media links.
- * You might need to add new Social media buttons or remove Facebook/Whatsapp/Messenger.
+ * TODO: You might need to add new Social media buttons or remove Facebook/Whatsapp/Messenger.
  * If so, import social media icons (or remove unused ones) under public/ dir.
  */
-export function getSocialMediaProps(dynamicContent: {
-  [key: string]: string;
-}): SocialMediaProps[] {
+export function getSocialMediaProps(
+  socialMediaLinks: SocialMediaLinks
+): SocialMediaProps[] {
   return [
     {
-      title: dynamicContent['default_banner_facebook_title'],
-      // TODO: create Dynamic content with link to Facebook page
-      href: '', // dynamicContent['<site_prefix>_facebook_link']
+      ...socialMediaLinks.facebookLink,
       image: facebookImage,
     },
     {
-      title: dynamicContent['default_banner_whatsapp_title'],
-      // TODO: create Dynamic content with link to Whatsapp page
-      href: '', // dynamicContent['<site_prefix>_whatsapp_link']
+      ...socialMediaLinks.whatsappLink,
       image: whatsappImage,
     },
     {
-      title: dynamicContent['default_banner_messenger_title'],
-      // TODO: create Dynamic content with link to Messenger page
-      href: '', // dynamicContent['<site_prefix>_messenger_link']
+      ...socialMediaLinks.messengerLink,
       image: messengerImage,
     },
   ];
