339ccbecd60eab829760bfcae90c8880529e3561
Emilio Guisolfo <emilioguisolfo@gmail.com>
2022-09-23 16:52:09 -0300

    fix: modified e2e test

--- cypress/e2e/redirects.cy.ts
+++ cypress/e2e/redirects.cy.ts
@@ -1,16 +1,12 @@
 describe('Redirects', () => {
-  // TODO Once you've connected a Zendesk CMS, provide a valid locale and
-  // article ID for e2e testing.
-  /*
-   *it('Legacy article form slug should navigate to the localized article page', () => {
-   *  cy.visit(
-   *    encodeURI(
-   *      'http://localhost:3003/hc/en-us/articles/4810060561175-some-slug'
-   *    )
-   *  );
-   *  cy.url().should('eq', 'http://localhost:3003/en-us/4810060561175');
-   *});
-   */
+  it('Legacy article form slug should navigate to the localized article page', () => {
+    cy.visit(
+      encodeURI(
+        'http://localhost:3003/hc/en-us/articles/6386200588061-Work-Culture-in-Greece'
+      )
+    );
+    cy.url().should('eq', 'http://localhost:3003/en-us/articles/6386200588061');
+  });
   it('Default home page visit should redirect to locale select', () => {
     cy.visit(encodeURI('http://localhost:3003'));
     // '%2F' corresponds to '/'.
