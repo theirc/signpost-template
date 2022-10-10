b551ff98760940a7fa431abc45a2cb1644e9dd10
Emilio Guisolfo <emilioguisolfo@gmail.com>
2022-10-10 16:11:54 -0300

    feat: added assets

--- lib/logo.ts
+++ lib/logo.ts
@@ -1,16 +1,26 @@
 import type { LogoProps } from '@ircsignpost/signpost-base/dist/src/header';
 import type { Locale } from '@ircsignpost/signpost-base/dist/src/locale';
 
-import logoEnUs from '../public/todo-logo-en-us.png';
+import logoEnUs from '../public/logo-en-us.png';
+import logoFa from '../public/logo-fa.png';
+import logoFr from '../public/logo-fr.png';
 
 export const getHeaderLogoProps = (currentLocale: Locale): LogoProps => {
-  let imgSrc: string;
+  let imgSrc: string = logoEnUs.src;
+
   switch (currentLocale.url) {
+    case 'fa':
+      imgSrc = logoFa.src;
+      break;
+    case 'fr':
+      imgSrc = logoFr.src;
+      break;
     case 'en-us':
     default:
       imgSrc = logoEnUs.src;
       break;
   }
+
   return {
     src: imgSrc,
     width: 110,
