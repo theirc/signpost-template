d440ff807f9004cd89076a1b3092a43556a8f7ae
Emilio Guisolfo <emilioguisolfo@gmail.com>
2022-11-08 17:01:46 -0300

    fix: changed dynamic content locale code

--- lib/constants.ts
+++ lib/constants.ts
@@ -47,7 +47,7 @@ export const CATEGORIES_TO_HIDE: number[] = [
 // https://developer.zendesk.com/api-reference/ticketing/account-configuration/locales/
 // Keep in sync with locales configured in /next.config.js.
 export const DYNAMIC_CONTENT_LOCALES: { [key: string]: number } = {
-  es: 1547,
+  es: 2,
 };
 
 export const ZENDESK_AUTH_HEADER = {
