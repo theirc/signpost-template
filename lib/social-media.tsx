9728f7a792e35d4919d1a8a1fa7208633aa263a3
bgSignpost <132521292+bgSignpost@users.noreply.github.com>
2024-04-02 10:59:00 -0400

    Update social-media.tsx

--- lib/social-media.tsx
+++ lib/social-media.tsx
@@ -4,7 +4,7 @@ import { SocialMediaProps } from '@ircsignpost/signpost-base/dist/src/header-ban
 import facebookImage from '../public/facebook.svg';
 import messengerImage from '../public/messenger.svg';
 import telegramImage from '../public/telegram.svg';
-import whatsAppChannelImage from ''../public/wachannel.svg''
+import whatsAppChannelImage from ''../public/wachannel.svg';
 
 export interface SocialMediaLink {
   title: string;
