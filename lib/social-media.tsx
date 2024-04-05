2c1ceab9d047a0e84e02ee8a43e7d31cf45aea80
bgSignpost <132521292+bgSignpost@users.noreply.github.com>
2024-04-05 10:21:16 -0400

    Update social-media.tsx

--- lib/social-media.tsx
+++ lib/social-media.tsx
@@ -36,6 +36,7 @@ export function getSocialMediaProps(
       ...socialMediaLinks.messengerLink,
       image: messengerImage,
     },
+    {
       ...socialMediaLinks.whatsAppLink,
       image: whatsAppImage,
     },
